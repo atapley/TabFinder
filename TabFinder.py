a9032a1618995d52bf60930943df4ec8d0aac4e2
Alex Tapley <tapleyalx@gmail.com>
2018-01-08 13:03:55 -0500

    Update TabFinder.py

--- TabFinder.py
+++ TabFinder.py
@@ -8,13 +8,13 @@ SoundRecorder.record()
 SONG_DETAILS = SongRecog.recog()
 FORM = ''
 
-if type(SONG_DETAILS) != str:
-    print('Error: Song not recognized or recording not found')
-else:
+if SONG_DETAILS != '' and type(SONG_DETAILS) == str:
+    print('Searching for ' + SONG_DETAILS + '...')
     CHORDS_LINK = ChordFind.chordFind(SONG_DETAILS)
     TABS_LINK = SongsterrFind.songsterrFind(SONG_DETAILS)
 
 def finalForm():
+    global FORM
     if CHORDS_LINK == '' and TABS_LINK == '':
         print('No chords or tabs could be found!')
     elif CHORDS_LINK == '':
@@ -25,23 +25,24 @@ def finalForm():
         FORM = input("Chords and tabs were found. Type 'CHORDS' to see chords, 'TABS' to see tabs: ")
 
 def retrieve():
+    global FORM
+
+    if FORM.lower() == 'chords':
+        FINAL = CHORDS_LINK
+    elif FORM.lower() == 'tabs':
+        FINAL = TABS_LINK
+    else:
+        print('Could not recognize input, try again')
         finalForm()
-
         if FORM.lower() == 'chords':
-            final = CHORDS_LINK
+            FINAL = CHORDS_LINK
         elif FORM.lower() == 'tabs':
-            final = TABS_LINK
+            FINAL = TABS_LINK
         else:
-            print('Could not recognize input, try again')
-            finalForm()
-            if FORM.lower() == 'chords':
-                final = CHORDS_LINK
-            elif FORM.lower() == 'tabs':
-                final = TABS_LINK
-            else:
-                print('Could not recognize input')
+            print('Could not recognize input')
+    webbrowser.open(FINAL)
 
-retrieve()
-
-
-
+if SONG_DETAILS != '' and type(SONG_DETAILS) == str:
+    finalForm()
+    if FORM != '':
+        retrieve()
